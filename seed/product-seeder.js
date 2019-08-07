5a68bbea8ee48253239e52fd7ae2b51e9e9e69f6
jobunski <jonyango003@gmail.com>
2019-08-07 23:30:49 +0300

    Demo Cart Page

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -11,10 +11,16 @@ var product = [
         price: 500
     }),
     new Product({
-        imagePath: 'https://images.unsplash.com/photo-1474031317822-f51f48735ddd?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-        title: 'Hoodie',
-        description: 'DUde under the hood. A Boogie with a hoodie!',
-        price: 500
+        imagePath: 'https://images.unsplash.com/photo-1457545195570-67f207084966?ixlib=rb-1.2.1&auto=format&fit=crop&w=1369&q=80',
+        title: 'Scarfs',
+        description: 'Woolen knitted scarfs!',
+        price: 300
+    }),
+    new Product({
+        imagePath: 'https://images.unsplash.com/photo-1483103068651-8ce44652c331?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        title: 'Baseball cap',
+        description: 'Summer themed baseball coat',
+        price: 200
     }),
     new Product({
         imagePath: 'https://images.unsplash.com/photo-1474031317822-f51f48735ddd?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
@@ -23,22 +29,16 @@ var product = [
         price: 500
     }),
     new Product({
-        imagePath: 'https://images.unsplash.com/photo-1474031317822-f51f48735ddd?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-        title: 'Hoodie',
-        description: 'DUde under the hood. A Boogie with a hoodie!',
-        price: 500
+        imagePath: 'https://images.unsplash.com/photo-1457545195570-67f207084966?ixlib=rb-1.2.1&auto=format&fit=crop&w=1369&q=80',
+        title: 'Scarfs',
+        description: 'Woolen knitted scarfs!',
+        price: 300
     }),
     new Product({
-        imagePath: 'https://images.unsplash.com/photo-1474031317822-f51f48735ddd?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-        title: 'Hoodie',
-        description: 'DUde under the hood. A Boogie with a hoodie!',
-        price: 500
-    }),
-    new Product({
-        imagePath: 'https://images.unsplash.com/photo-1474031317822-f51f48735ddd?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80',
-        title: 'Hoodie',
-        description: 'DUde under the hood. A Boogie with a hoodie!',
-        price: 500
+        imagePath: 'https://images.unsplash.com/photo-1483103068651-8ce44652c331?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        title: 'Baseball cap',
+        description: 'Summer themed baseball coat',
+        price: 200
     })
 ]
 
